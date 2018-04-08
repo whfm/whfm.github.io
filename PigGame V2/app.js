a22a62527384009869356c4a937990d7fb3342d7
walte <walte@WALTER720>
2018-04-07 23:13:20 -0400

    V2

--- PigGame V2/app.js
+++ PigGame V2/app.js
@@ -54,7 +54,17 @@ document.querySelector('.btn-hold').addEventListener('click', function () {
         scores[activePlayer] += roundScore;
         document.querySelector('#score-' + activePlayer).textContent = scores[activePlayer];
 
-        if (scores[activePlayer] >= 100) {
+        var input = document.querySelector('.final-score').value;
+        var winningScore;
+
+        if (input) {
+            winningScore = input;
+        }
+        else {
+            winningScore = 100;
+        }
+
+        if (scores[activePlayer] >= winningScore) {
             document.querySelector('#name-' + activePlayer).textContent = 'Winner!';
             document.querySelector('.dice').style.display = 'none';
             document.querySelector('.player-' + activePlayer + '-panel').classList.add('winner');
